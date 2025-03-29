b8c75fbb63907c8099e55cfda007a69904908c68
Bader Zoabi <bader.zoabi@gmail.com>
2025-03-29 22:04:43 +0200

    create different components for Header, Post, Posts, SideMenu and integrate them within the APP

---
+++ src/Post.js
@@ -0,0 +1,9 @@
+export default function Post() {
+  return (
+    <div className="Post">
+      <h2>This is the post title</h2>
+      <hr></hr>
+      <div style={{ textAlign: "center" }}>This is the post content</div>
+    </div>
+  );
+}
---
+++ src/SideMenu.js
@@ -0,0 +1,25 @@
+export default function SideMenu() {
+  return (
+    <div className="SideMenu">
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+      <TagButton />
+    </div>
+  );
+}
+
+function TagButton() {
+  return <button className="TagButton">Tag Button</button>;
+}
