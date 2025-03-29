749567acba0017b88be083d402e1e66536b57887
Bader Zoabi <bader.zoabi@gmail.com>
2025-03-29 23:29:30 +0200

    send children props as the body of the post using JSX

--- src/Posts.js
+++ src/Posts.js
@@ -3,10 +3,17 @@ import Post from "./Post";
 export default function Posts() {
   return (
     <div className="Posts">
-      {/* Title with Body */}
-      <Post title="Title 1" body="Body 1" />
+      {/* Title with Children */}
+      <Post title="Title 1">
+        <h2>This is the content of the first post</h2>
+      </Post>
       {/* Empty title */}
-      <Post body="Body 2" />
+      <Post>
+        <h1 style={{ background: "Black", color: "white" }}>
+          Black and White content with button:
+        </h1>
+        <button>Click me!!</button>
+      </Post>
       {/* Empty body */}
       <Post title="Title 3" />
     </div>
