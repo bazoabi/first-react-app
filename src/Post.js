044c1bc88630994df0c4ee771733ac2706f3f1a5
Bader Zoabi <bader.zoabi@gmail.com>
2025-03-29 23:06:27 +0200

    add props: title and body to the posts

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,13 @@
-export default function Post() {
+export default function Post({ title, body }) {
   return (
     <div className="Post">
-      <h2>This is the post title</h2>
+      <h2 style={{ textAlign: "center" }}>
+        {title ?? "This is the default post title"}
+      </h2>
       <hr></hr>
-      <div style={{ textAlign: "center" }}>This is the post content</div>
+      <div style={{ textAlign: "center" }}>
+        {body ?? "This is the default post body"}
+      </div>
     </div>
   );
 }
